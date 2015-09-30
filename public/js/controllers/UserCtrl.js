72a99e6bfb23e5fbe15692baf874cf64a1c1fa6c
Ricardo Machado <ricardomachado.sb@gmail.com>
2015-09-29 23:33:52 -0300

    Added messages to user layout actions

--- public/js/controllers/UserCtrl.js
+++ public/js/controllers/UserCtrl.js
@@ -23,8 +23,14 @@ angular.module('UserCtrl', []).controller('UserController', function($scope, $ht
 			function(res){
 				$scope.user = "";
 				$scope.list();
+
+				$scope.messageClass = 'alert-success';
+				$scope.message = 'Usuário cadastrado';
 			}
-		);
+		).error(function(res){
+			$scope.messageClass = 'alert-danger';
+			$scope.message = 'Problemas ao cadastrar usuário';
+		});
 	};
 
 	$scope.put = function(id){
@@ -33,19 +39,30 @@ angular.module('UserCtrl', []).controller('UserController', function($scope, $ht
 			function(res){
 				$scope.user = "";
 				$scope.list();
-			}
 
-		);
+				$scope.messageClass = 'alert-success';
+				$scope.message = 'Usuário alterado';
+			}
+		).error(function(res){
+			$scope.messageClass = 'alert-danger';
+			$scope.message = 'Problemas ao cadastrar usuário';
+		});
 	}
 
 	$scope.remove = function( id ){
 		$http.delete('/api/user/remove/' + id, {headers: $rootScope.tokenHeader}).success(
 
 			function(res){
+				$scope.user = "";
 				$scope.list();
-			}
 
-		);
+				$scope.messageClass = 'alert-success';
+				$scope.message = 'Usuário deletado';
+			}
+		).error(function(res){
+			$scope.messageClass = 'alert-danger';
+			$scope.message = 'Problemas ao deletar Usuário';
+		});
 	};
 
 	$scope.edit = function( e ){
