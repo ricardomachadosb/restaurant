dc6f03c6a1e02fff539f04c5b6ee590f56524fbc
Ricardo Machado <ricardomachado.sb@gmail.com>
2015-12-19 12:49:59 -0200

    Adding dish to order

--- back/models/order.js
+++ back/models/order.js
@@ -1,9 +1,15 @@
 module.exports = function(app){
 	var mongoose = require('mongoose');
 
+  var OrderDishSchema = new mongoose.Schema({ dish:{type: mongoose.Schema.ObjectId, ref: 'Dish' },
+    quantity:{type: Number, default: 0 }, observation: {Type: String}
+  });
+
+
 	return mongoose.model('Order', {
       code : {type : String},
       status: {type: Boolean, default: false},
-      tables: [{type : mongoose.Schema.ObjectId, ref : 'Table'}]
+      tables: [{type : mongoose.Schema.ObjectId, ref : 'Table'}],
+      dishes: [OrderDishSchema]
 	});
 }
