f86b36862e95b40097abf1c990a3908af8916f31
Ricardo Machado <ricardomachado.sb@gmail.com>
2016-01-09 10:09:21 -0200

    Fixing bug at close bill layout, NaN value at tot item column to dish

--- back/models/drink.js
+++ back/models/drink.js
@@ -4,13 +4,13 @@ module.exports = function(app){
 //	var picture =
 //	var Mixed = mongoose.Schema.Types.Mixed;
 //new mongoose.Schema({ _user: Mixed })
-	
+
 	return mongoose.model('Drink', {
       code : {type : String},
       price : {type : Number},
       name : {type : String, default: ''},
       description : {type : String, default: ''},
-	  picture : [],
+      picture : [],
       isVisible : {type : Boolean, default: false}
 	});
 }
